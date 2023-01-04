02a31214dc9454fe7a15161a48b765f1950a7319
Seshasai Pamulapati <hemaseshasai.griffin@gmail.com>
2023-01-03 19:47:18 -0600

    added assignment 6

---
+++ Assignment6/assig.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Person struct {
+	firstname  string
+	lastname   string
+	favCountry []string
+}
+type vehicle struct {
+	doors int
+	color string
+}
+type truck struct {
+	fourWheel bool
+	vehicle
+}
+type sedan struct {
+	luxury bool
+	vehicle
+}
+type SQUARE struct {
+	length float64
+	width  float64
+}
+type CIRCLE struct {
+	radius float64
+}
+type SHAPE interface {
+	Area() float64
+}
+
+func main() {
+
+	m := map[string]Person{}
+	pSlice := []Person{
+		Person{firstname: "sai",
+			lastname: "pamulapati",
+			favCountry: []string{
+				"usa",
+				"india",
+				"uk",
+			},
+		},
+		Person{firstname: "Amar",
+			lastname: "murukutla",
+			favCountry: []string{
+				"vietnam",
+				"bankkock",
+				"uk",
+			},
+		},
+	}
+	outputOfSlice(pSlice)
+	mappingSliceData(pSlice, m)
+	Truck := truck{
+		fourWheel: true,
+		vehicle: vehicle{
+			doors: 4,
+			color: "yellow",
+		},
+	}
+	Sedan := sedan{
+		luxury: true,
+		vehicle: vehicle{
+			doors: 2,
+			color: "black",
+		},
+	}
+	fmt.Println(Truck)
+	fmt.Println(Truck.color)
+	fmt.Println(Truck.doors)
+	fmt.Println(Truck.fourWheel)
+	fmt.Println(Sedan)
+	fmt.Println(Sedan.color)
+	fmt.Println(Sedan.doors)
+	fmt.Println(Sedan.luxury)
+
+	info(CIRCLE{radius: 9.00})
+	info(SQUARE{length: 9.11, width: 8.22})
+
+}
+func outputOfSlice(pSlice []Person) {
+
+	for c, a := range pSlice {
+		if c == 0 {
+			fmt.Println("First person struct ", a)
+		} else {
+			fmt.Println("Second  person struct ", a)
+		}
+		fmt.Println("Person First Name", a.firstname)
+		fmt.Println("Looping over the favourite Country")
+		for c, a := range a.favCountry {
+			fmt.Println(c, a)
+		}
+	}
+
+}
+func mappingSliceData(pSlice []Person, m map[string]Person) {
+	for _, a := range pSlice {
+		m[a.lastname] = a
+
+	}
+
+}
+func (s SQUARE) Area() float64 {
+	return s.length * s.width
+}
+func (c CIRCLE) Area() float64 {
+	return math.Pi * (c.radius * c.radius)
+}
+
+func info(s SHAPE) {
+	fmt.Println(s)
+	fmt.Println(s.Area())
+}
